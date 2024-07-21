8172be1cac9835f23cd0c3cb5e84a7a060de20fe
namjimin3123832 <mimi3123832@gmail.com>
2024-07-21 20:59:11 +0900

    Comparing numbers

---
+++ algorithm_codecata_5.py
@@ -0,0 +1,10 @@
+# 5번)
+# 정수 num1과 num2가 매개변수로 주어집니다.
+# 두 수가 같으면 1 다르면 -1을 retrun하도록 solution 함수를 완성해주세요.
+
+def solution(num1, num2):
+    if num1 == num2:     # num1과 num2가 같다면
+        return 1         # 1을 리턴하고
+
+    else:                # num1과 num2가 다르다면
+        return -1        # -1을 리턴
